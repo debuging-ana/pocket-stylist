d91ec1b19d93dbf96b05f968a5cf558253a5865a
Dineth02 <dinethbeneragama@gmail.com>
2025-04-09 13:54:37 +1200

    Dineth First Commit: no changes to project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+//imports relevant files
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
