d1cd3ee580baab5d51b415ae92f4d1f9403552ba
debuging-ana <maisanacarolina@gmail.com>
2025-05-01 19:50:05 +1200

    adding new pages to start the navegation in the app

--- app/_layout.js
+++ app/_layout.js
@@ -1,25 +1,78 @@
-import { Stack } from 'expo-router';
+
 import BackButton from '../components/BackButton';
+import { Tabs } from 'expo-router';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerLeft: () => <BackButton />, //our custom back button
-        headerBackTitleVisible: false, //for iOS specific which hides the back text (shows arrow)
-        headerStyle: {
-          backgroundColor: '#AFC6A3',
-        },
-        headerTitleStyle: {
-          color: 'white',
-          fontWeight: '600',
-        },
-        headerTintColor: 'white', 
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ title: 'Log In' }} />
-      <Stack.Screen name="signup" options={{ title: 'Create Account' }} />
-    </Stack>
+    <Tabs screenOptions={{
+      headerLeft: () => <BackButton />,
+      headerBackTitleVisible: false,
+      headerStyle: {
+        backgroundColor: '#AFC6A3',
+      },
+      headerTitleStyle: {
+        color: 'white',
+        fontWeight: '600',
+      },
+      headerTintColor: 'white',
+    }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Feather name="home" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="location"
+        options={{
+          title: 'Location',
+          tabBarIcon: ({ color }) => <Entypo name="location" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: 'Camera',
+          tabBarIcon: ({ color }) => <Feather name="camera" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="wardrobe"
+        options={{
+          title: 'Wardrobe',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="hanger" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <Ionicons name="settings-outline" size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="login"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="signup"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="profile/index"
+        options={{
+          href: null,
+        }}
+      />
+    </Tabs>
   );
 }
